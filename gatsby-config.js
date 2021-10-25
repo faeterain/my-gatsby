dfb5bd9534ff199758db9df7bdf75aebdd2b95cc
Nguyễn Tấn Phát - CMC Global DU11 <ntphat@cmcglobal.vn>
2021-10-25 09:40:42 +0700

    Add smoothscroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-smoothscroll`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react'
 import Layout from '../components/layout';
+import scrollTo from 'gatsby-plugin-smoothscroll';
+import './index.css'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is my home page</p>
+      <button onClick={() => scrollTo('#adog')}>Dog</button>
+      <button onClick={() => scrollTo('#sphinx')}>Cat</button>
+      <StaticImage id="adog" alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large" />
+      <StaticImage id="sphinx" alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/sphynx.jpg" />
     </Layout>
   )
 }
